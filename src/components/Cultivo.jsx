b3950a30c47a995c4d79e8a25f9d9668152c5876
luis fernando enciso <encisolf901@gmail.com>
2021-05-22 15:13:02 -0500

    demo 2

--- src/components/Cultivo.jsx
+++ src/components/Cultivo.jsx
@@ -13,8 +13,6 @@ class Cultivo extends Component{
                         <h5 className="card-title">{this.props.cultivo_name}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                         <p className="card-text">sss</p>
-                        <a  className="card-link">Card link</a>
-                        <a  className="card-link">Another link</a>
                     </div>
                 </div>                
             </Fragment>
--- src/components/Lote.jsx
+++ src/components/Lote.jsx
@@ -14,8 +14,8 @@ class Lote extends Component{
                         <h5 className="card-title">{this.props.lote_name}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a className="card-link">Card link</a>
-                        <a  className="card-link">Another link</a>
+                        
+                        
                     </div>
                 </div>  
             </Fragment>
